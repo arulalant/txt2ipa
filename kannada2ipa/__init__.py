d5c3cd0a0e1f3fed8d915ac63c68118997968203
Arulalan <arulalant@gmail.com>
2015-04-26 19:25:45 +0530

    update

--- kannada2ipa/__init__.py
+++ kannada2ipa/__init__.py
@@ -1,2 +1,4 @@
-from encode2unicode import *
-from unicode2encode import *
+# (C) 2014 Arulalan.T <arulalant@gmail.com>                                 
+# This file is part of txt2ipa
+
+from ipaconvert import kannada2ipa
