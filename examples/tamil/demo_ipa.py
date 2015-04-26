b86441ac9bdf3969675a74e47547c247352e0399
Arulalan <arulalant@gmail.com>
2015-04-26 19:25:30 +0530

    update

--- examples/tamil/demo_ipa.py
+++ examples/tamil/demo_ipa.py
@@ -3,12 +3,12 @@
 
 # (C) 2014 Arulalan.T <arulalant@gmail.com>
 # 
-# This file is part of 'open-tamil/txt2ipa' package examples
+# This file is part of 'txt2ipa' package examples
 # 
 
 
-from txt2ipa.ipaconvert import ipa, broad
-from txt2ipa.transliteration import tam2lat
+from tamil2ipa.ipaconvert import ipa, broad
+from tamil2ipa.transliteration import tam2lat
 
 
 text = 'வணக்கம் தமிழகம் '
